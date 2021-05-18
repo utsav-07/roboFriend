5544878080fe848a3269b2614d7191db3d8b175c
unknown <divyansh8252230@gmail.com>
2021-05-18 22:58:43 +0530

    Update

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Card = (props) => {
     return (
         
-            <div className='bg-light-green dib br3 pa3 ma2 frow bw2 shadow-5'>
+            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
                 <img alt='robots' src ='https://robohash.org//test?200x200'/>
                 <div>
                     <h2>{props.name}</h2>
